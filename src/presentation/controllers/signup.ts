60298f71c05f01236a15bb9b89601f5c3a56bcc3
Antonio Francisco <afpb04@hotmail.com>
2022-03-17 17:59:35 -0300

    refactor: :recycle: ensure SignUpController.handle implement correct interfaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,7 @@
+import { IHttpRequest, IHttpResponse } from '../protocols/http';
+
 export class SignUpController {
-  handle(httpRequest: any): any {
+  handle(httpRequest: IHttpRequest): IHttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
@@ -14,6 +16,7 @@ export class SignUpController {
     }
     return {
       statusCode: 200,
+      body: 'Success',
     };
   }
 }
