cc4b5aec9277af097d19589833193cf312228cfb
Antonio Francisco <afpb04@hotmail.com>
2022-03-29 18:08:29 -0300

    refactor: :recycle: remove code duplication

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -26,10 +26,12 @@ export class SignUpController implements IController {
           return badRequest(new MissingParamError(field));
         }
       }
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      const { email, password, passwordConfirmation } = httpRequest.body;
+
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'));
       }
-      const isValid = this.emailValidation.isValid(httpRequest.body.email);
+      const isValid = this.emailValidation.isValid(email);
 
       if (!isValid) {
         return badRequest(new InvalidParamError('email'));
